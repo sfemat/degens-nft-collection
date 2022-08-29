89a882c1947ae92bfb40e522f04090c7b0444ee9
sfemat <sfemat@libero.it>
2022-08-29 23:54:51 +0200

    Initial

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xD3DBE37D459DF350091fde3752Ab3750b229e679";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,36 @@
+//  0xCbED072907f0a802d33D13c139eC6268D711E0df
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const DegensContract = await ethers.getContractFactory("Degens");
+
+  // deploy the contract
+  const deployedDegensContract = await DegensContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  // print the address of the deployed contract
+  console.log(
+    "Degens Contract Address:",
+    deployedDegensContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
